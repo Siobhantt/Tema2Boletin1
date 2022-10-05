eb097c533462fb3890941d72802294451ee5b746
lcardozo <lcardozo@106-25.info2>
2022-10-05 09:28:31 +0200

    (:

--- Tema2Boletin/src/tema2boletin/Tema2Ejercicio4.java
+++ Tema2Boletin/src/tema2boletin/Tema2Ejercicio4.java
@@ -14,17 +14,20 @@ public class Tema2Ejercicio4 {
 		//Solicitamos los numeros
 		System.out.println("Por favor introduzca los numeros: ");
 		
-		System.out.print("1): ");
+		System.out.print("Primer numero: ");
 		numero1 = lee.nextInt();
-		System.out.print("2): ");
+		System.out.print("Segundo numero: ");
 		numero2 = lee.nextInt();
 		
-		if (numero1 > numero2) {
+		if (numero1 < numero2) {
 			System.out.print(numero1 + ", " + numero2);
 		}
 		else {
 			System.out.println(numero2 + ", " + numero1);
 		}
+		
+		//Cerramos el scanner
+		lee.close();
 		// TODO Auto-generated method stub
 
 	}
